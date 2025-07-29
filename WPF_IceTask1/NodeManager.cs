caaa2b2e55b4ada8bf09d7ed04579120df428cd8
Aliziwe Qeqe <st10382076@vcconnect.edu.za>
2025-07-29 12:03:22 +0200

    populateList compiled

--- WPF_IceTask1/NodeManager.cs
+++ WPF_IceTask1/NodeManager.cs
@@ -10,20 +10,20 @@ namespace WPF_IceTask1
     {
         public Node Head;
 
-        public void Add(int number)
+        public void Add(int number, string contents) //this adds the 'chapter number' into the node
         {
-            if (number == null)
+            if (number == null && contents == null)
             {
                 return;
             }
             else if (Head == null)
             {
-                Head = new Node(number);
+                Head = new Node(number,contents);
                 return;
             }
             else
             {
-                Node n = new Node(number);
+                Node n = new Node(number,contents);
                 Node current = Head;
                 while (current.next != null)
                 {
@@ -35,3 +35,6 @@ namespace WPF_IceTask1
         }
     }
 }
+//dma overfull
+//check if it's working on his side - scanning image 
+//write out what you're doing and start figuring it out
